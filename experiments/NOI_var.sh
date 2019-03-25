61296c0006c511210143bdf5f03a4e9ff4053c92
Rien Lagerwerf <rienlagerwerf@gmail.com>
2019-03-25 15:42:13 +0100

    SIRT with nonnegativity in the experiments

---
+++ experiments/NOI_var.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+# limit numper of OpenMP threads
+#export OMP_NUM_THREADS=16
+# set astra gpu index: 0-3
+#export CUDA_VISIBLE_DEVICES=2,3
+
+
+for i in {0..5}
+do
+    python NOI_var.py -p -F \
+    NNFDK_results/NOI_var_1024 with it_i=$i
+done
+
---
+++ experiments/SV_var.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+# limit numper of OpenMP threads
+# export OMP_NUM_THREADS=16
+# set astra gpu index: 0-3
+# export CUDA_VISIBLE_DEVICES=0,1
+
+
+for i in {0..4}
+do
+    python SV_var.py -p -F \
+    NNFDK_results/SV_var_1024 with it_i=$i
+done
+
