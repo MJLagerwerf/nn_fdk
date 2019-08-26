fc9dd9bb370270d7defa2988a8c1ced151fa69fb
Rien Lagerwerf <rienlagerwerf@gmail.com>
2019-08-26 12:35:49 +0200

    Added NOI SV combo bash script

---
+++ experiments/NOI_SV_var.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+# limit numper of OpenMP threads
+#export OMP_NUM_THREADS=16
+# set astra gpu index: 0-3
+#export CUDA_VISIBLE_DEVICES=2,3
+
+
+for i in {0..9}
+do
+    python NOI_var.py -p -F \
+    NNFDK_results/NOI_var_1024 with it_i=$i pix=1024
+done
+
+for i in {0..8}
+do
+    python SV_var.py -p -F \
+    NNFDK_results/SV_var_1024 with it_i=$i pix=1024
+done
