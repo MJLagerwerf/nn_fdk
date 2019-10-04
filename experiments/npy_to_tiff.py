df4aafb9432778a72b7f2e7f2d457dec79b4bb5d
Rien Lagerwerf <rienlagerwerf@gmail.com>
2019-10-04 15:49:50 +0200

    npy to tiffs RD5

--- experiments/npy_to_tiff.py
+++ experiments/npy_to_tiff.py
@@ -15,18 +15,18 @@ lpath = '/export/scratch2/lagerwer/NNFDK_results/FDK_RD/'
 path1 = '/export/scratch2/lagerwer/data/FleXray/walnuts_10MAY/walnut_'
 path2 = '/tiffs/'
 
-dset = 'good'
-AF = 8
+dset = 'noisy'
+AF = 1
 rfiles = [4 * i + 1 for i in range(21)]
 dfiles = ['{:02d}'.format(1 + i) for i in range(21)]
-for i in range(21):
-    arr = np.load(f'{lpath}{rfiles[i]}/RD_{dset}_AF{AF}_FDKHN_obj.npy')
-    sp = f'{path1}{dfiles[i]}/{dset}_AF{AF}{path2}FDK/'
-#    print(arr)
-#    print(sp)
-    os.makedirs(sp)
-    for i in tqdm(range(np.size(arr, axis=-1))):        
-        io.imsave('{}stack_{:05d}.tiff'.format(sp, i), arr[:, :, i])
+#for i in range(21):
+#    arr = np.load(f'{lpath}{rfiles[i]}/RD_{dset}_AF{AF}_FDKHN_obj.npy')
+#    sp = f'{path1}{dfiles[i]}/{dset}_AF{AF}{path2}FDK/'
+##    print(arr)
+##    print(sp)
+#    os.makedirs(sp)
+#    for i in tqdm(range(np.size(arr, axis=-1))):        
+#        io.imsave('{}stack_{:05d}.tiff'.format(sp, i), arr[:, :, i])
 
 
 for f in dfiles:
