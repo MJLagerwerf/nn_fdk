d8a29b6090a91b400712a105773d54f58686d231
Rien Lagerwerf <rienlagerwerf@gmail.com>
2019-10-04 15:54:52 +0200

    npy to tiffs RD6 + run extra MSD on noisy RD

--- experiments/npy_to_tiff.py
+++ experiments/npy_to_tiff.py
@@ -15,24 +15,26 @@ lpath = '/export/scratch2/lagerwer/NNFDK_results/FDK_RD/'
 path1 = '/export/scratch2/lagerwer/data/FleXray/walnuts_10MAY/walnut_'
 path2 = '/tiffs/'
 
-dset = 'noisy'
-AF = 1
-rfiles = [4 * i + 1 for i in range(21)]
+dset = 'good'
+AF = 8
+rfiles = [4 * i + 2 for i in range(21)]
 dfiles = ['{:02d}'.format(1 + i) for i in range(21)]
-#for i in range(21):
-#    arr = np.load(f'{lpath}{rfiles[i]}/RD_{dset}_AF{AF}_FDKHN_obj.npy')
-#    sp = f'{path1}{dfiles[i]}/{dset}_AF{AF}{path2}FDK/'
-##    print(arr)
-##    print(sp)
-#    os.makedirs(sp)
-#    for i in tqdm(range(np.size(arr, axis=-1))):        
-#        io.imsave('{}stack_{:05d}.tiff'.format(sp, i), arr[:, :, i])
+for i in tqdm(range(21)):
+    arr = np.load(f'{lpath}{rfiles[i]}/RD_{dset}_AF{AF}_FDKHN_obj.npy')
+    sp = f'{path1}{dfiles[i]}/{dset}_AF{AF}{path2}FDK/'
+#    print(arr)
+#    print(sp)
+    os.makedirs(sp)
+    for i in (range(np.size(arr, axis=-1))):        
+        io.imsave('{}stack_{:05d}.tiff'.format(sp, i), arr[:, :, i])
 
 
-for f in dfiles:
-    lpath = f'{path1}{f}/processed_data/'
+
+for i in tqdm(range(21)):
+    lpath = f'{path1}{dfiles[i]}/processed_data/'
     arr = np.load(f'{lpath}ground_truth.npy')
-    sp = f'{path1}{f}/{dset}_AF{AF}{path2}GS/'
+    sp = f'{path1}{dfiles[i]}/{dset}_AF{AF}{path2}GS/'
+    
 #    print(arr)
 #    print(sp)
     os.makedirs(sp)
