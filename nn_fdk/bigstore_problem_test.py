66195aef1bdebfe3a43339851ff38576150065cb
Rien Lagerwerf <rienlagerwerf@gmail.com>
2019-12-20 15:30:06 +0100

    bigstore problems?2

--- nn_fdk/bigstore_problem_test.py
+++ nn_fdk/bigstore_problem_test.py
@@ -8,8 +8,20 @@ Created on Fri Dec 20 15:21:09 2019
 
 import msdnet 
 from tqdm import tqdm
+import gc
 
-save_path = '/bigstore//lagerwer/data/NNFDK/DF_V1024_A360_SR2/L2/MSD/nTD10nVD5/'
+
+save_path = '/bigstore//lagerwer/data/NNFDK/DF_V1024_A360_SR2/L2/MSD/'
 
 for i in tqdm(range(10000)):
-    n = msdnet.network.MSDNet.from_file(f'{save_path}regr_params.h5',gpu=True)
+    if i % 3 == 0:
+        n = msdnet.network.MSDNet.from_file(f'{save_path}nTD1nVD0/regr_params.h5',
+                                            gpu=True)
+    if i % 3 == 1:
+        n = msdnet.network.MSDNet.from_file(f'{save_path}nTD1nVD1/regr_params.h5',
+                                            gpu=True)
+    if i % 3 == 2:
+        n = msdnet.network.MSDNet.from_file(f'{save_path}nTD10nVD5/regr_params.h5',
+                                            gpu=True)
+    n = None
+    gc.collect()
