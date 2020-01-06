2980c2a2cb580fa2dbbe8cf631a1bd6fe7be1c9b
Rien Lagerwerf <rienlagerwerf@gmail.com>
2020-01-06 14:02:23 +0100

    Updated the FDK RD script16

--- experiments/npy_to_tiff.py
+++ experiments/npy_to_tiff.py
@@ -12,17 +12,18 @@ import os
 from tqdm import tqdm
 # %%
 lpath = '/bigstore/lagerwer/NNFDK_results/FDK_RD/'
-path1 = '/bigstore/lagerwer/data/FleXray/walnuts_10MAY/walnut_'
+path1 = '/bigstore/lagerwer/data/FleXray/Walnuts/Walnut'
 path2 = '/tiffs/'
 
 
 AF = 1
-dset = 'noisy'
+dset = 'tubeV2'
 rfiles = [2 * i + 1 for i in range(21)]
-dfiles = ['{:02d}'.format(1 + i) for i in range(21)]
+#dfiles = ['{:02d}'.format(1 + i) for i in range(21)]
+dfiles = [i + 1 for i in range(21)]
 for i in tqdm(range(21)):
     arr = np.load(f'{lpath}{rfiles[i]}/RD_{dset}_AF{AF}_FDKHN_obj.npy')
-    sp = f'{path1}{dfiles[i]}/processed_data{path2}{dset}/FDK/'
+    sp = f'{path1}{dfiles[i]}Projections/processed_data{path2}{dset}/FDK/'
 #    sp = f'{path1}{dfiles[i]}/{dset}{path2}FDK/'
 #    print(arr)
 #    print(sp)
@@ -30,13 +31,13 @@ for i in tqdm(range(21)):
     for i in (range(np.size(arr, axis=-1))):        
         io.imsave('{}stack_{:05d}.tiff'.format(sp, i), arr[:, :, i])
 
-#for i in tqdm(range(21)):
-#    lpath = f'{path1}{dfiles[i]}/processed_data/'
-#    arr = np.load(f'{lpath}ground_truth.npy')
-#    sp = f'{path1}{dfiles[i]}/processed_data{path2}GS/'
-##    sp = f'{path1}{dfiles[i]}/{dset}{path2}GS/'
-##    print(arr)
-##    print(sp)
-#    os.makedirs(sp)
-#    for i in range(np.size(arr, axis=-1)):        
-#        io.imsave('{}stack_{:05d}.tiff'.format(sp, i), arr[:, :, i])
+for i in tqdm(range(21)):
+    lpath = f'{path1}{dfiles[i]}/processed_data/'
+    arr = np.load(f'{lpath}ground_truth.npy')
+    sp = f'{path1}{dfiles[i]}Projections/processed_data{path2}GS/'
+#    sp = f'{path1}{dfiles[i]}/{dset}{path2}GS/'
+#    print(arr)
+#    print(sp)
+    os.makedirs(sp)
+    for i in range(np.size(arr, axis=-1)):        
+        io.imsave('{}stack_{:05d}.tiff'.format(sp, i), arr[:, :, i])
