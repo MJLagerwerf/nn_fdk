51fbd148154d15497162d4df94885a66b45ae640
Rien Lagerwerf <rienlagerwerf@gmail.com>
2020-01-06 12:18:48 +0100

    Updated the FDK RD script7

--- experiments/npy_to_tiff.py
+++ experiments/npy_to_tiff.py
@@ -15,13 +15,14 @@ lpath = '/bigstore/lagerwer/NNFDK_results/FDK_RD/'
 path1 = '/bigstore/lagerwer/data/FleXray/walnuts_10MAY/walnut_'
 path2 = '/tiffs/'
 
-dset = 'good'
-AF = 16
-rfiles = [2 * i + 2 for i in range(21)]
+
+AF = 1
+dset = 'noisy'
+rfiles = [2 * i + 1 for i in range(21)]
 dfiles = ['{:02d}'.format(1 + i) for i in range(21)]
 for i in tqdm(range(21)):
     arr = np.load(f'{lpath}{rfiles[i]}/RD_{dset}_AF{AF}_FDKHN_obj.npy')
-    sp = f'{path1}{dfiles[i]}/processed_data{path2}{dset}_AF{AF}/FDK/'
+    sp = f'{path1}{dfiles[i]}/processed_data{path2}{dset}/FDK/'
 #    sp = f'{path1}{dfiles[i]}/{dset}{path2}FDK/'
 #    print(arr)
 #    print(sp)
@@ -29,13 +30,13 @@ for i in tqdm(range(21)):
     for i in (range(np.size(arr, axis=-1))):        
         io.imsave('{}stack_{:05d}.tiff'.format(sp, i), arr[:, :, i])
 
-for i in tqdm(range(21)):
-    lpath = f'{path1}{dfiles[i]}/processed_data/'
-    arr = np.load(f'{lpath}ground_truth.npy')
-    sp = f'{path1}{dfiles[i]}/processed_data{path2}GS/'
-#    sp = f'{path1}{dfiles[i]}/{dset}{path2}GS/'
-#    print(arr)
-#    print(sp)
-    os.makedirs(sp)
-    for i in range(np.size(arr, axis=-1)):        
-        io.imsave('{}stack_{:05d}.tiff'.format(sp, i), arr[:, :, i])
+#for i in tqdm(range(21)):
+#    lpath = f'{path1}{dfiles[i]}/processed_data/'
+#    arr = np.load(f'{lpath}ground_truth.npy')
+#    sp = f'{path1}{dfiles[i]}/processed_data{path2}GS/'
+##    sp = f'{path1}{dfiles[i]}/{dset}{path2}GS/'
+##    print(arr)
+##    print(sp)
+#    os.makedirs(sp)
+#    for i in range(np.size(arr, axis=-1)):        
+#        io.imsave('{}stack_{:05d}.tiff'.format(sp, i), arr[:, :, i])
