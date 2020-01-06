c66e120cab31dbf16b436d88a10940df9f766823
Rien Lagerwerf <rienlagerwerf@gmail.com>
2020-01-06 13:25:01 +0100

    Updated the FDK RD script9

--- experiments/exp_make_FDKs_RD.sh
+++ experiments/exp_make_FDKs_RD.sh
@@ -4,13 +4,13 @@
 # set astra gpu index: 0-3
 #export CUDA_VISIBLE_DEVICES=2,3
 
-for i in {01..21}
+for i in {1..21}
 do
-    for j in {0..1}
-    do
-        python exp_make_FDKs_RD.py -p -F  \
-        /bigstore/lagerwer/NNFDK_results/FDK_RD/ with it_j=$j it_i=$i
-    done
+    python exp_make_FDKs_RD.py -p -F  \
+    /bigstore/lagerwer/NNFDK_results/FDK_RD/ with it_i=$i
 done
 
+#    for j in {0..1}
+#    do
 
+#    done
