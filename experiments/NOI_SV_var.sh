2a779db1d75bfcc295e0c4fbe2559539e4443eb3
Rien Lagerwerf <rienlagerwerf@gmail.com>
2020-01-21 10:27:46 +0100

    redo exp noi

--- experiments/NOI_SV_var.sh
+++ experiments/NOI_SV_var.sh
@@ -5,22 +5,28 @@
 # export CUDA_VISIBLE_DEVICES=3
 
 
-for i in {0..4}
+#for i in {0..4}
+#do
+#    CUDA_VISIBLE_DEVICES=3 \
+#    python SV_var.py -p -F \
+#    /export/scratch2/lagerwer/NNFDK_results/SV_var_1024 with it_i=$i pix=1024 nTD=10 nVD=5
+#done
+
+for i in {0..5}
 do
     CUDA_VISIBLE_DEVICES=3 \
-    python SV_var.py -p -F \
-    /export/scratch2/lagerwer/NNFDK_results/SV_var_1024 with it_i=$i pix=1024 nTD=10 nVD=5
+    python NOI_var.py -p -F \
+    /export/scratch2/lagerwer/NNFDK_results/NOI_var_1024_S1 with it_i=$i pix=1024 nTD=1 nVD=0
 done
 
 for i in {0..5}
 do
     CUDA_VISIBLE_DEVICES=3 \
     python NOI_var.py -p -F \
-    /export/scratch2/lagerwer/NNFDK_results/NOI_var_1024 with it_i=$i pix=1024 nTD=10 nVD=5
+    /export/scratch2/lagerwer/NNFDK_results/NOI_var_1024_S2 with it_i=$i pix=1024 nTD=1 nVD=1
 done
-
-for i in 2 3 5 7.5 10
-do
-    CUDA_VISIBLE_DEVICES=3 \
-    python exp_cone_angle.py -p with src_rad=$i pix=1024 nTD=10 nVD=5
-done
+#for i in 2 3 5 7.5 10
+#do
+#    CUDA_VISIBLE_DEVICES=3 \
+#    python exp_cone_angle.py -p with src_rad=$i pix=1024 nTD=10 nVD=5
+#done
