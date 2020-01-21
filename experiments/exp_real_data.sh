6a26ea2392039ff97406db91b0bca8cf8eb712b2
Rien Lagerwerf <rienlagerwerf@gmail.com>
2020-01-21 09:55:45 +0100

    new S3 case also for real data3

--- experiments/exp_real_data.sh
+++ experiments/exp_real_data.sh
@@ -4,17 +4,17 @@
 # set astra gpu index: 0-3
 #export CUDA_VISIBLE_DEVICES=2,3
 
-for i in {0..3}
-do
-    CUDA_VISIBLE_DEVICES=2 \
-    python exp_real_data.py -p -F \
-    NNFDK_results/real_data_S3 with it_i=$i nTD=10 nVD=5
-done
+#for i in {0..3}
+#do
+#    CUDA_VISIBLE_DEVICES=2 \
+#    python exp_real_data.py -p -F \
+#    NNFDK_results/real_data_S3 with it_i=$i nTD=10 nVD=5
+#done
 
 CUDA_VISIBLE_DEVICES=2 python exp_real_data_CA.py -p -F \ 
-NNFDK_results/real_data_CA_S1 with it_i=$i nTD=1 nVD=0
+NNFDK_results/RD_CA_S1 with it_i=$i nTD=1 nVD=0
 CUDA_VISIBLE_DEVICES=2 python exp_real_data_CA.py -p -F \ 
-NNFDK_results/real_data_CA_S2 with it_i=$i nTD=1 nVD=1
+NNFDK_results/RD_CA_S2 with it_i=$i nTD=1 nVD=1
 CUDA_VISIBLE_DEVICES=2 python exp_real_data_CA.py -p -F \
-NNFDK_results/real_data_CA_S3 with it_i=$i nTD=10 nVD=5
+NNFDK_results/RD_CA_S3 with it_i=$i nTD=10 nVD=5
 
