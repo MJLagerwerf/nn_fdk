dcc299ff0a18d628f08768657eac0dc6c9fc296b
Rien Lagerwerf <rienlagerwerf@gmail.com>
2020-01-21 11:01:36 +0100

    new S3 case also for real data6

--- experiments/exp_real_data.sh
+++ experiments/exp_real_data.sh
@@ -11,10 +11,7 @@
 #    NNFDK_results/real_data_S3 with it_i=$i nTD=10 nVD=5
 #done
 
-CUDA_VISIBLE_DEVICES=2 python exp_RD_CA.py -p -F \ 
-NNFDK_results/real_data_CA_S1 with it_i=$i nTD=1 nVD=0
-CUDA_VISIBLE_DEVICES=2 python exp_RD_CA.py -p -F \ 
-NNFDK_results/real_data_CA_S2 with it_i=$i nTD=1 nVD=1
-CUDA_VISIBLE_DEVICES=2 python exp_RD_CA.py -p -F \
-NNFDK_results/real_data_CA_S3 with it_i=$i nTD=10 nVD=5
+CUDA_VISIBLE_DEVICES=2 python exp_RD_CA.py -p -F NNFDK_results/real_data_CA_S1 with it_i=$i nTD=1 nVD=0
+CUDA_VISIBLE_DEVICES=2 python exp_RD_CA.py -p -F NNFDK_results/real_data_CA_S2 with it_i=$i nTD=1 nVD=1
+CUDA_VISIBLE_DEVICES=2 python exp_RD_CA.py -p -F NNFDK_results/real_data_CA_S3 with it_i=$i nTD=10 nVD=5
 
