6927dec58b1c0aed61f72664d5c5f994f680d635
Rien Lagerwerf <rienlagerwerf@gmail.com>
2020-02-12 16:29:18 +0100

    Added a bash script

---
+++ experiments/exp_DNN_stats.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+# limit numper of OpenMP threads
+#export OMP_NUM_THREADS=16
+# set astra gpu index: 0-3
+#export CUDA_VISIBLE_DEVICES=0,1,2,3
+
+for i in {0..20}
+do
+    python exp_DNNs_recons.py -p -F \
+    NNFDK_results/DNN_stats_4S with phantom='Fourshape'
+done
+
+for i in {0..20}
+do
+    python exp_DNNs_recons.py -p -F \
+    NNFDK_results/DNN_stats_DF with phantom='Defrise random'
+done
+
