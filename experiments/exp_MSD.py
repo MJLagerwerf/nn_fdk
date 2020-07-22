7eaa63a14e454a2a8983b8bd006d7f62d9fc8fb2
Rien Lagerwerf <rienlagerwerf@gmail.com>
2020-07-22 10:44:32 +0200

    Updated the U-net and MSD-net experiments4

--- experiments/exp_MSD.py
+++ experiments/exp_MSD.py
@@ -65,7 +65,7 @@ def cfg():
         rads = [2, 3, 5, 7.5, 10]
         src_rad = rads[it_i]
     train = True
-    stop_crit = 50
+    stop_crit = 200
 
     # Specifics for the expansion operator
     Exp_bin = 'linear'
@@ -78,7 +78,7 @@ def cfg():
 def main(pix, phantom, nTD, nTrain, nVD, nVal, train, bpath, stop_crit,
          PH, angles, src_rad, det_rad, noise, Exp_bin, bin_param):
     # Specific phantom
-    
+
     
     # %%
     t1 = time.time()
@@ -89,40 +89,49 @@ def main(pix, phantom, nTD, nTrain, nVD, nVal, train, bpath, stop_crit,
           'seconds')
     
     # %% Create a test phantom
+
     voxels = [pix, pix, pix]
     # Create a data object
     t2 = time.time()
-    data_obj = ddf.phantom(voxels, phantom, angles, noise, src_rad, det_rad)#,
-    #                       compute_xHQ=True)
-    print('Making phantom and mask took', time.time() -t2, 'seconds')
-    # The amount of projection angles in the measurements
-    # Source to center of rotation radius
-     
-    t3 = time.time()
-    # %% Create the circular cone beam CT class
-    case = ddf.CCB_CT(data_obj)#, angles, src_rad, det_rad, noise)
-    print('Making data and operators took', time.time()-t3, 'seconds')
-    # Initialize the algorithms (FDK, SIRT)
-    t4 = time.time()
-    case.init_algo()
-    
-    # %% Create NN-FDK algorithm setup
-    # Create binned filter space and expansion operator
-    spf_space, Exp_op = ddf.support_functions.ExpOp_builder(bin_param,
-                                                         case.filter_space,
-                                                         interp=Exp_bin)
-    # Create the FDK binned operator
-    case.FDK_bin_nn = case.FDK_op * Exp_op
-    
-    # Create the NN-FDK object
-    case.NNFDK = nn.NNFDK_class(case, nTrain, nTD, nVal, nVD, Exp_bin, Exp_op,
-                                 bin_param, base_path=bpath)
-    case.rec_methods += [case.NNFDK]
+    if not train:
+        data_obj = ddf.phantom(voxels, phantom, angles, noise, src_rad,
+                               det_rad)#,
+        #                       compute_xHQ=True)
+        print('Making phantom and mask took', time.time() -t2, 'seconds')
+        # The amount of projection angles in the measurements
+        # Source to center of rotation radius
+         
+        t3 = time.time()
+        # %% Create the circular cone beam CT class
+        case = ddf.CCB_CT(data_obj)#, angles, src_rad, det_rad, noise)
+        print('Making data and operators took', time.time()-t3, 'seconds')
+        # Initialize the algorithms (FDK, SIRT)
+        t4 = time.time()
+        case.init_algo()
+        
+        # %% Create NN-FDK algorithm setup
+        # Create binned filter space and expansion operator
+        spf_space, Exp_op = ddf.support_functions.ExpOp_builder(bin_param,
+                                                             case.filter_space,
+                                                             interp=Exp_bin)
+        # Create the FDK binned operator
+        case.FDK_bin_nn = case.FDK_op * Exp_op
+        
+        # Create the NN-FDK object
+        case.NNFDK = nn.NNFDK_class(case, nTrain, nTD, nVal, nVD, Exp_bin,
+                                    Exp_op,
+                                     bin_param, base_path=bpath)
+        case.rec_methods += [case.NNFDK]
+        case.MSD = msd.MSD_class(case, case.NNFDK.data_path)
+        case.rec_methods += [case.MSD]
+    else:
+        data_path = nn.make_data_path(pix, phantom, angles, src_rad, noise,
+                                      Exp_bin, bin_param, base_path=bpath)
+        MSD = msd.MSD_class(None, data_path)
     print('Initializing algorithms took', time.time() - t4, 'seconds')
     
     # %%
-    case.MSD = msd.MSD_class(case, case.NNFDK.data_path)
-    case.rec_methods += [case.MSD]
+
     
     l_tr, l_v = nn.Preprocess_datasets.random_lists(nTD, nVD)
     if nVD == 0:
@@ -137,7 +146,7 @@ def main(pix, phantom, nTD, nTrain, nVD, nVal, train, bpath, stop_crit,
         
     if train:
         print('Started training function')
-        case.MSD.train(list_tr, list_v, stop_crit=stop_crit, ratio=3)
+        MSD.train(list_tr, list_v, stop_crit=stop_crit, ratio=3)
     
     else:
         case.MSD.add2sp_list(list_tr, list_v)
--- experiments/exp_Unet.py
+++ experiments/exp_Unet.py
@@ -64,8 +64,8 @@ def cfg():
         rads = [2, 3, 5, 7.5, 10]
         src_rad = rads[it_i]
     train = True
-    stop_crit = 50
-    epochs = 10_000
+    stop_crit = 200
+    epochs = 1
     # Specifics for the expansion operator
     Exp_bin = 'linear'
     bin_param = 2
@@ -86,43 +86,46 @@ def main(pix, phantom, nTD, nTrain, nVD, nVal, train, bpath, stop_crit, epochs,
                                      base_path=bpath)
     print('Creating training and validation datasets took', time.time() - t1,
           'seconds')
-    
-    # %% Create a test phantom
-    voxels = [pix, pix, pix]
-    # Create a data object
-    t2 = time.time()
-    data_obj = ddf.phantom(voxels, phantom, angles, noise, src_rad, det_rad)#,
-    #                       compute_xHQ=True)
-    print('Making phantom and mask took', time.time() -t2, 'seconds')
-    # The amount of projection angles in the measurements
-    # Source to center of rotation radius
-     
-    t3 = time.time()
-    # %% Create the circular cone beam CT class
-    case = ddf.CCB_CT(data_obj)#, angles, src_rad, det_rad, noise)
-    print('Making data and operators took', time.time()-t3, 'seconds')
-    # Initialize the algorithms (FDK, SIRT)
-    t4 = time.time()
-    case.init_algo()
-    
-    # %% Create NN-FDK algorithm setup
-    # Create binned filter space and expansion operator
-    spf_space, Exp_op = ddf.support_functions.ExpOp_builder(bin_param,
-                                                            case.filter_space,
-                                                            interp=Exp_bin)
-    # Create the FDK binned operator
-    case.FDK_bin_nn = case.FDK_op * Exp_op
-    
-    # Create the NN-FDK object
-    case.NNFDK = nn.NNFDK_class(case, nTrain, nTD, nVal, nVD, Exp_bin, Exp_op,
-                                 bin_param, base_path=bpath)
-    case.rec_methods += [case.NNFDK]
-    print('Initializing algorithms took', time.time() - t4, 'seconds')
-    
-    # %%
-    case.Unet = unet.Unet_class(case, case.NNFDK.data_path)
-    case.rec_methods += [case.Unet]
-    
+    if not train:
+        # %% Create a test phantom
+        voxels = [pix, pix, pix]
+        # Create a data object
+        t2 = time.time()
+        data_obj = ddf.phantom(voxels, phantom, angles, noise, src_rad, det_rad)#,
+        #                       compute_xHQ=True)
+        print('Making phantom and mask took', time.time() -t2, 'seconds')
+        # The amount of projection angles in the measurements
+        # Source to center of rotation radius
+         
+        t3 = time.time()
+        # %% Create the circular cone beam CT class
+        case = ddf.CCB_CT(data_obj)#, angles, src_rad, det_rad, noise)
+        print('Making data and operators took', time.time()-t3, 'seconds')
+        # Initialize the algorithms (FDK, SIRT)
+        t4 = time.time()
+        case.init_algo()
+        
+        # %% Create NN-FDK algorithm setup
+        # Create binned filter space and expansion operator
+        spf_space, Exp_op = ddf.support_functions.ExpOp_builder(bin_param,
+                                                                case.filter_space,
+                                                                interp=Exp_bin)
+        # Create the FDK binned operator
+        case.FDK_bin_nn = case.FDK_op * Exp_op
+        
+        # Create the NN-FDK object
+        case.NNFDK = nn.NNFDK_class(case, nTrain, nTD, nVal, nVD, Exp_bin, Exp_op,
+                                     bin_param, base_path=bpath)
+        case.rec_methods += [case.NNFDK]
+        print('Initializing algorithms took', time.time() - t4, 'seconds')
+        
+        # %%
+        case.Unet = unet.Unet_class(case, case.NNFDK.data_path)
+        case.rec_methods += [case.Unet]
+    else:
+        data_path = nn.make_data_path(pix, phantom, angles, src_rad, noise,
+                                      Exp_bin, bin_param, base_path=bpath)
+        Unet = unet.Unet_class(None, data_path)
     if nVD == 0:
         list_tr = [0]
         list_v = None
@@ -135,7 +138,7 @@ def main(pix, phantom, nTD, nTrain, nVD, nVal, train, bpath, stop_crit, epochs,
     
     if train:
         print('training')
-        case.Unet.train(list_tr, list_v, epochs=epochs, stop_crit=stop_crit, 
+        Unet.train(list_tr, list_v, epochs=epochs, stop_crit=stop_crit, 
                         ratio=3)
     else:
         print(f'Reconstructing...')
