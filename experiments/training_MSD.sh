12dc29ba5c9fc6731af3f8b9b288d0b0d237517a
Rien Lagerwerf <rienlagerwerf@gmail.com>
2020-08-11 11:05:25 +0200

    Updated training shell scripts

--- experiments/training_MSD.sh
+++ experiments/training_MSD.sh
@@ -5,32 +5,29 @@ read timer_bool
 echo "Give GPU index"
 read GPU_i
 
-echo "Give array of indices, yea this is stupid, but cannot think of another way"
-read -a ind
-
 echo "Give experiment types in array form with ' and spaces in between"
 read -a ExpT
 
-echo "Experiment iterations in array form with spaces between"
-read -a ExpI
+echo "Experiment iteration"
+read ExpI
 
-for i in ${ind[@]}; 
+for i in 0 1 2; 
 do
 	if [ $timer_bool == 'y' ]; then 
 		# Experiment that I want to run
-		CUDA_VISIBLE_DEVICES=${GPU_i} python exp_MSD.py -p with exp_type=${ExpT[$i} it_i=${ExpI[$i]} --log=WARNING &
+		CUDA_VISIBLE_DEVICES=${GPU_i} python exp_MSD.py -p with exp_type=${ExpT[$i} it_i=${ExpI} --log=WARNING &
 		# Sleeps how much time you need:
 		sleep 2d
 
 		# Kills all python processes:
 		nvidia-smi | grep 'python' | awk '{ print $3 }' | xargs -n1 kill -9
 
-		echo "Stopped training of exp type: ${ExpT[$i]}, with it_i: ${ExpI[$i]}, MSD"
+		echo "Stopped training of exp type: ${ExpT[$i]}, with it_i: ${ExpI}, MSD"
 	else
 		# Experiment that I want to run
-		CUDA_VISIBLE_DEVICES=${GPU_i} python exp_MSD.py -p with exp_type=${ExpT[$i} it_i=${ExpI[$i]} --log=WARNING
+		CUDA_VISIBLE_DEVICES=${GPU_i} python exp_MSD.py -p with exp_type=${ExpT[$i} it_i=${ExpI} --log=WARNING
 
-		echo "Stopped training of exp type: ${ExpT[$i]}, with it_i: ${ExpI[$i]}, MSD"
+		echo "Stopped training of exp type: ${ExpT[$i]}, with it_i: ${ExpI}, MSD"
 	fi
 done
 
--- experiments/training_Unet.sh
+++ experiments/training_Unet.sh
@@ -5,30 +5,27 @@ read timer_bool
 echo "Give GPU index"
 read GPU_i
 
-echo "Give array of indices, yea this is stupid, but cannot think of another way"
-read -a ind
-
 echo "Give experiment types in array form with ' and spaces in between"
 read -a ExpT
 
-echo "Experiment iterations in array form with spaces between"
-read -a ExpI
+echo "Experiment iteration"
+read ExpI
 
 for i in ${ind[@]}; 
 do
 	if [ $timer_bool == 'y' ]; then 
 		# Experiment that I want to run
-		CUDA_VISIBLE_DEVICES=${GPU_i} python exp_Unet.py -p with exp_type=${ExpT[$i} it_i=${ExpI[$i]} &
+		CUDA_VISIBLE_DEVICES=${GPU_i} python exp_Unet.py -p with exp_type=${ExpT[$i} it_i=${ExpI} &
 		# Sleeps how much time you need:
 		sleep 2d
 
 		# Kills all python processes:
 		nvidia-smi | grep 'python' | awk '{ print $3 }' | xargs -n1 kill -9
 
-		echo "Stopped training of exp type: ${ExpT[$i]}, with it_i: ${ExpI[$i]}, Unet"
+		echo "Stopped training of exp type: ${ExpT[$i]}, with it_i: ${ExpI}, Unet"
 	else
 		# Experiment that I want to run
-		CUDA_VISIBLE_DEVICES=${GPU_i} python exp_Unet.py -p with exp_type=${ExpT[$i} it_i=${ExpI[$i]} 
+		CUDA_VISIBLE_DEVICES=${GPU_i} python exp_Unet.py -p with exp_type=${ExpT[$i} it_i=${ExpI} 
 
 		echo "Stopped training of exp type: ${ExpT[$i]}, with it_i: ${ExpI[$i]}, Unet"
 	fi
