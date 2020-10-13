701b087de2dc2632f189fd7941ee893e721d1903
Rien Lagerwerf <rienlagerwerf@gmail.com>
2020-10-13 11:15:12 +0200

    Cleaned up example script

--- examples/example_simulated_data.py
+++ examples/example_simulated_data.py
@@ -13,41 +13,35 @@ import nn_fdk as nn
 import time
 import pylab
 t = time.time()
+
 # %%
-def make_hann_filt(voxels, w_detu):
-    rs_detu = int(2 ** (np.ceil(np.log2(voxels[0] * 2)) + 1))
-    filt = np.real(np.fft.rfft(ddf.ramp_filt(rs_detu)))
-    freq = 2 * np.arange(len(filt))/(rs_detu)
-    filt = filt * (np.cos(freq * np.pi / 2) ** 2)  / 2 / w_detu
-#    filt = filt / 2 / w_detu
-    return filt
-# %%
-pix = 1024
+pix = 512
 # Specific phantom
 phantom = 'Fourshape_test'
 # Number of angles
-angles = 32
+angles = 360
 # Source radius
 src_rad = 10
 det_rad = 0
 # Noise specifics
-noise = None #['Poisson', 2 ** 8]
+noise = ['Poisson', 2 ** 8]
 # Number of voxels used for training, number of datasets used for training
-nTrain, nTD = 1e6, 10
+nTrain, nTD = 1e6, 1
 # Number of voxels used for validation, number of datasets used for validation
-nVal, nVD = 1e6, 5
+nVal, nVD = 1e6, 1
 
 # Specifics for the expansion operator
 Exp_bin = 'linear'
 bin_param = 2
 
+# Set a path where you want to store your data
 bpath = '/bigstore/lagerwer/data/NNFDK/'
-# bpath = '/export/scratch2/lagerwer/data/NNFDK/'
-#'/export/scratch1/home/voxels-gpu0/data/NNFDK/'
+
+
 # %%
 t1 = time.time()
 nn.Create_TrainingValidationData(pix, phantom, angles, src_rad, noise,
-                                 Exp_bin, bin_param, nTD + nVD, base_path=bpath)
+                                  Exp_bin, bin_param, nTD + nVD, base_path=bpath)
 print('Creating training and validation datasets took', time.time() - t1,
       'seconds')
 
@@ -55,18 +49,12 @@ print('Creating training and validation datasets took', time.time() - t1,
 voxels = [pix, pix, pix]
 # Create a data object
 t2 = time.time()
-data_obj = ddf.phantom(voxels, phantom, angles, noise, src_rad, det_rad)#,
-#                       compute_xHQ=True)
+data_obj = ddf.phantom(voxels, phantom, angles, noise, src_rad, det_rad)
 print('Making phantom and mask took', time.time() -t2, 'seconds')
-# The amount of projection angles in the measurements
-# Source to center of rotation radius
-# filt = make_hann_filt(data_obj.voxels, data_obj.w_detu)
-# xFDK = ddf.FDK_ODL_astra_backend.FDK_astra(data_obj.g, filt,
-#                                            data_obj.geometry, 
-#                                            data_obj.reco_space, None)
+
 t3 = time.time()
 # %% Create the circular cone beam CT class
-case = ddf.CCB_CT(data_obj)#, angles, src_rad, det_rad, noise)
+case = ddf.CCB_CT(data_obj)
 print('Making data and operators took', time.time()-t3, 'seconds')
 # Initialize the algorithms (FDK, SIRT)
 t4 = time.time()
@@ -75,31 +63,27 @@ case.init_algo()
 # %% Create NN-FDK algorithm setup
 # Create binned filter space and expansion operator
 spf_space, Exp_op = ddf.support_functions.ExpOp_builder(bin_param,
-                                                     case.filter_space,
-                                                     interp=Exp_bin)
+                                                      case.filter_space,
+                                                      interp=Exp_bin)
 # Create the FDK binned operator
 case.FDK_bin_nn = case.FDK_op * Exp_op
 
 # Create the NN-FDK object
 case.NNFDK = nn.NNFDK_class(case, nTrain, nTD, nVal, nVD, Exp_bin, Exp_op,
-                             bin_param, base_path=bpath)
+                              bin_param, base_path=bpath)
 case.rec_methods += [case.NNFDK]
 print('Initializing algorithms took', time.time() - t4, 'seconds')
-# %%
 
-
-# case.FDK.do('Hann')
 # %%
-case.NNFDK.train(4, retrain=True, save_model_pb=True)
+case.show_phantom()
+
+case.FDK.do('Hann')
+case.FDK.show()
+
+case.NNFDK.train(4, retrain=True)
 case.NNFDK.do()
 case.NNFDK.show()
-# # %%
-# #pylab.close('all')
-# case.table()
-# case.show_phantom()
-# #case.MSD.show(clim=False)
-# #case.show_xHQ()
-# case.NNFDK.show()
-#case.NNFDK.show_filters()
-#case.NNFDK.show_node_output(3)
+
+# %%
+case.table()
 
